36a5698efcf969c939441015e34daf953cd8fb26
saurabh24gope <saurabhgope@gmail.com>
2022-12-13 21:38:57 +0530

    SU-3 Create Spring Boot application with required dependencies

---
+++ src/test/java/com/shruteekatech/usermanagementapp/UserManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shruteekatech.usermanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
