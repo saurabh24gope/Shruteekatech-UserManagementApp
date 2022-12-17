a3a42e9ace9391775beb25cf290ab61b87b322e6
saurabh24gope <saurabhgope@gmail.com>
2022-12-17 07:32:16 +0530

    SU-5 Create required packages in the project

---
+++ src/main/java/com/shruteekatech/usermanagementapp/entities/City.java
@@ -0,0 +1,5 @@
+package com.shruteekatech.usermanagementapp.entities;
+
+public class City {
+
+}
---
+++ src/main/java/com/shruteekatech/usermanagementapp/repositories/CountryRepository.java
@@ -0,0 +1,5 @@
+package com.shruteekatech.usermanagementapp.repositories;
+
+public class CountryRepository {
+
+}
