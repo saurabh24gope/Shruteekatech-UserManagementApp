9310cba56c2669904123ae4d0eb02ca048ffffaa
saurabh24gope <saurabhgope@gmail.com>
2022-12-17 14:28:30 +0530

    Create Entitiy classes for 4 tables-user_accounts,city_master,state_master,country_master

--- src/main/java/com/shruteekatech/usermanagementapp/entities/City.java
+++ src/main/java/com/shruteekatech/usermanagementapp/entities/City.java
@@ -1,5 +1,18 @@
 package com.shruteekatech.usermanagementapp.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="city_master")
 public class City {
+	@Column(name="city_id")
+	private int cityId;
+	@Column(name="city_name")
+	private int stateName;
+	@Column(name="city_stateid")
+	private int stateId;
 
 }
