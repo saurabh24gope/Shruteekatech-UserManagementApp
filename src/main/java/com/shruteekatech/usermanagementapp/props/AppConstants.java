1b84f62c1c7431176e271dd864ebcf65f0b6f1a9
saurabh24gope <saurabhgope@gmail.com>
2022-12-18 07:36:37 +0530

    SU-12 Create AppConstants.java class to declare constant variables

---
+++ src/main/java/com/shruteekatech/usermanagementapp/props/AppConstants.java
@@ -0,0 +1,9 @@
+package com.shruteekatech.usermanagementapp.props;
+
+public class AppConstants {
+	
+	public static final String SAVE_SUCESS ="saveSucess";
+	public static final String SAVE_FAIL ="saveFail";
+	
+
+}
